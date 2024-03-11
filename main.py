67a730724225638759f5ad29e35ecf95163d9f45
Jaconda <jaconda@studying.com>
2024-03-11 23:45:11 +0300

    done row and column func and need fix bug with row

--- main.py
+++ main.py
@@ -9,36 +9,41 @@ def check_saddle_point(matr):
     return alpha == betta
 
 
-def crossing_out_rows():
-    pass
+def crossing_out_rows(mtx):
+    if len(mtx) >= 2:
+        for row in range(len(mtx) - 1):
+            temp1 = mtx[row]
+            for row2 in range(row + 1, len(mtx)):
+                temp2 = mtx[row2]
+                if all([x <= y for x, y in zip(temp1, temp2)]):
+                    mtx.pop(row)
+                    return crossing_out_rows(mtx)
+                elif all([x >= y for x, y in zip(temp1, temp2)]):
+                    mtx.pop(row2)
+                    return crossing_out_rows(mtx)
+
+        return mtx
 
 
-def crossing_out_columns():
-    pass
+def crossing_out_columns(mtx):
+    if len(mtx) >= 2:
+        for elem in range(len(mtx[0]) - 1):
+            temp_arr = [x[elem] for x in mtx]
+            for elem2 in range(elem + 1, len(mtx[0])):
+                temp_arr2 = [x[elem2] for x in mtx]
+                if all([x <= y for x, y in zip(temp_arr, temp_arr2)]):
+                    for i in range(len(mtx)):
+                        mtx[i].pop(elem2)
+                    return crossing_out_columns(mtx)
+                elif all([x >= y for x, y in zip(temp_arr, temp_arr2)]):
+                    for j in range(len(mtx)):
+                        mtx[j].pop(elem)
+                    return crossing_out_columns(mtx)
 
-
-# def crossing_out(matr):
-#     temp_list = list()
-#     len_matr = len(matr)
-#     len_column = len(matr[0])
-#     matr_copy = matr.copy()
-# 
-#     for i in range(len(matr) - 1):
-#         for j in range(i, len(matr)):
-#             temp1 = matr[i]
-#             temp2 = matr[j]
-#             if all([x <= y for x, y in zip(temp1, temp2)]):
-#                 matr_copy.remove(matr[i])
-#             elif all([x >= y for x, y in zip(temp1, temp2)]):
-#                 matr_copy.remove(matr[j])
-#     return matr_copy
+    return mtx
 
 
 if __name__ == '__main__':
-
-    # 1 2 3
-    # 4 7 9
-    # 1 2 3
 
     # 4 -2 5 1 2 7
     # 1 2 4 3 0 10
@@ -48,9 +53,15 @@ if __name__ == '__main__':
 
     matrix = list()
     k = int(input("Введите количество строк в матрице: "))
-    for i in range(k):
+    for _ in range(k):
         matrix.append(list(map(int, input().split())))
     if not check_saddle_point(matrix):
-        print(crossing_out(matrix))
+        cross_row = crossing_out_rows(matrix)
+        cross_column = crossing_out_columns(matrix)
+        while cross_row != cross_column:
+            cross_row = crossing_out_rows(matrix)
+            cross_column = crossing_out_columns(matrix)
+        print(matrix)
+
     else:
         print("Седловая точка")
